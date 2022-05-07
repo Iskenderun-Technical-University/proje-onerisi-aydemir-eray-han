5cdaf5964c2d19797ae4576356f12f1fdb76f427
Aydemir Eray Han <erayhan41@yandex.com>
2022-05-07 17:30:08 +0300

    screen ui changes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./src/navigation";
-
+import * as eva from "@eva-design/eva";
+import {
+  ApplicationProvider,
+  IconRegistry,
+  Layout,
+} from "@ui-kitten/components";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Navigation />
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.dark}>
+        <Navigation />
+      </ApplicationProvider>
     </SafeAreaProvider>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,32 @@
-import React from "react";
-import { Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { Avatar, Layout, Text } from "@ui-kitten/components";
 import { SafeAreaView } from "react-native-safe-area-context";
 type Props = {};
 
 export default function Home({}: Props) {
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
+    <Layout style={styles.container}>
+      <SafeAreaView>
+        <Layout>
+          <Text category={"p1"}>Hosgeldin film sever</Text>
+          <Text category={"h3"}>Fragmania</Text>
+        </Layout>
+        <Layout>
+          <Avatar
+            size="giant"
+            source={{
+              uri: "https://www.themoviedb.org/t/p/w710_and_h400_multi_faces/gD830J0sf5gEeZvzkRVPdGxJmSR.jpg",
+            }}
+          />
+        </Layout>
+      </SafeAreaView>
+    </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
